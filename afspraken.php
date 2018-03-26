9b4fdf220b11eb9e42d0309485b230b31ce8c7ab
Mauro van Hal <maurovanhal@hotmail.com>
2018-03-26 14:25:34 +0200

    ksalksfhkhfsd
    kajsakl

--- afspraken.php
+++ afspraken.php
@@ -4,50 +4,37 @@
 <?php 
 
 $Naam=$_GET['naam'];
-$Woonplaats=$_GET['mail'];
-$Postcode=$_GET['datum'];
-$Lidmaatschapnummer=$_GET['tijd'];
-$geboortedatum=$_GET['behandeling'];
-$Email=$_GET['kapper'];
+$Mail=$_GET['mail'];
+$Datum=$_GET['datum'];
+$Tijd=$_GET['tijd'];
+$Behandeling=$_GET['behandeling'];
+$Kapper=$_GET['kapper'];
 
 echo $Naam;
-echo $;
-echo $Adres;
-echo $Woonplaats;
-echo $Postcode;
-echo $Lidmaatschapnummer;
-echo $geboortedatum;
-echo $Email;
-echo $Wachtwoord;
+echo $Mail;
+echo $Datum;
+echo $Tijd;
+echo $Behandeling;
+echo $Kapper;
 
     include("dbconnect.php");
 
     $query="INSERT INTO klanten(id,naam,woonplaats,postcode,telefoonnummer,mail) VALUES
     ('$id','$Naam','$Woonplaats','$Postcode','$Telefoonnummer','$Mail')";
 ?>
-<form action="welcome.php" method="post">
-Naam: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-Datum:  <input type="date" name="datum"></br>
-Tijd:   <input type="time" name="tijd" value="09:00" min="09:00:00" max="21:00:00"/></br>
-Behandeling:
-<select name="FormBehandeling">
-  <option value="">Select...</option>
-  <option value="knippen">Knippen</option>
-  <option value="kleuren">Kleuren</option>
-  <option value="blayage">Blayage</option>
-</select></br>
-Kapper:
-<select name="kapper">
-    <option value="">Select...</option>
-    <option value="brenda">Brenda</option>
-    <option value="maria">Maria</option>
-    <option value="gino">Gino</option>
-</select></br></br>
 
-<input type="submit">
-
-</form>
+<script type="text/javascript">
+setTimeout('Redirect()',10);
+function Redirect()
+{
+  location.href = 'home.html';
+}
+</script>
+<?php
+    echo '<script language="text/javascript">';
+    echo 'alert("Afspraak is gemaakt")';
+    echo '</scipt>';
+?>
 
 
 
---
+++ invoer-afspraken.php
@@ -0,0 +1,68 @@
+<html>
+        <head>
+            <title>invoer klant </title>      
+        </head>
+        <body>
+<?php
+    include("dbconnect.php");
+    $query="SELECT naamKappers FROM kappers";
+    $resultaat= mysqli_query($con, $query);
+    while($rij = mysqli_fetch_array($resultaat)){
+        $Brenda = $rij['Brenda'];
+        $Maria = $rij['Maria'];
+        $Gino = $rij['Gino'];
+    }
+?>
+
+<?php
+    include("dbconnect.php");
+    $query1="SELECT naamBehandeling FROM behandeling";
+    $resultaat1= mysqli_query($con, $query1);
+    while($rij1 = mysqli_fetch_array($resultaat1)){
+        $Knippen = $rij1['knippen'];
+        $Kleuren = $rij1['kleuren'];
+        $Balayage = $rij1['balayage'];
+        $highlight4 = $rij1['highlight-1/4'];
+        $highlight2 = $rij['highlight-1/2'];
+        $highlight1 = $rij['highlight-1/1'];
+    } 
+?>
+<form action="afspraken.php" method="GET">
+        <table>
+            <tr><td>Naam: </td>
+            <td><input name="naam"/></td></tr>
+            <tr><td>E-Mail: </td>
+            <td><input name="mail"/></td></tr>
+            <tr><td>Datum: </td>
+            <td><input type="date" name="datum"></td></tr>
+            <tr><td>Tijd: </td>
+            <td><input type="time" value="09:00" name="tijd" min="09:00:00" max="21:00:00"></td></tr>
+             <tr><td>Behandeling: </td>
+            <td><select name="FormBehandeling">
+                    <option value="">Select...</option>
+                    <option value="<?php echo $knippen ?>">Knippen</option>
+                    <option value="<?php echo $kleuren ?>">Kleuren</option>
+                    <option value="<?php echo $balayage ?>">Balayage</option>
+                    <option value="<?php echo $highlight4 ?>">Highlight 1/4</option>
+                    <option value="<?php echo $highlight2 ?>">Highlight 1/2</option>
+                    <option value="<?php echo $highlight1 ?>">Highlight 1/1</option>
+
+                  </select></td></tr>
+                  <tr><td>Kapper: </td>
+            <td><select name="kapper">
+                    <option value="">Select...</option>
+                    <option value="<?php echo $Brenda ?>">Brenda</option>
+                    <option value="<?php echo $Maria ?>">Maria</option>
+                    <option value="<?php echo $Gino ?>">Gino</option>
+                </select></td></tr>
+             </tr>
+             <tr>
+                <td colspan="2" >
+                    <input type ="submit" name="submit"/>
+                    <input type ="button"  value="naar overzicht" onclick="window.location.href= 'overzicht.html'"/>
+                </td>
+            </tr>                
+        </table>      
+    </form>  
+</body>
+</html>
