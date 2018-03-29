4b3ffc39dbb14bea4597b53bde74d6f7158ee90e
Mauro van Hal <maurovanhal@hotmail.com>
2018-03-29 09:03:19 +0200

    xaxaxaxaxa
    xaaxaxaxaxax

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+    include('home2.html');
+?>
+<html>
+    <head>
+        <title>Contact Us</title>
+    </head>
+    <body>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2439.6830985170927!2d4.68875411529826!3d52.303606079773765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5e7aed1d3471b%3A0xbc60809c9fe25310!2sTuinweg+7%2C+2132+DN+Hoofddorp!5e0!3m2!1sen!2snl!4v1522306735682" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </body>
+</html>
