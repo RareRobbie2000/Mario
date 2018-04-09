0c6b89eaedcf363cf7d6115567f5c95e517daa6b
Mauro van Hal <maurovanhal@hotmail.com>
2018-04-09 11:21:51 +0200

    contact page
    axaxaxaxaxaxa

--- contact.php
+++ contact.php
@@ -14,8 +14,7 @@
 </head>
 <body>
     <div class="main">
-		
-			<a class="navbar-brand" href="#">Mario</a>
+	<a class="navbar-brand" href="#">Mario</a>
 		
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
@@ -24,9 +23,16 @@
 				<li><a href="#">Team</a></li>
 				<li><a href="#">Contact</a></li>
 			</ul>
-		</div>					
-				<div id="screen">
-					<h1> Contact </h1>
+		</div>
+		
+
+	<div class="container">
+  <div class="row">
+    <div class="col-sm-7">
+	<iframe class="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2439.683098517092!2d4.688754115298267!3d52.30360607977378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5e7aed1d3471b%3A0xbc60809c9fe25310!2sTuinweg+7%2C+2132+DN+Hoofddorp!5e0!3m2!1snl!2snl!4v1523265279130" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="col-sm-5">
+	<h1> Contact </h1>
 					<br/>
 					<form>
 					
@@ -45,6 +51,13 @@
 			 </div>
 
 	</div>
+    </div>
+  </div>
+</div>
+
+								
+				<div id="screen">
+					
 	
 <footer>
  <!--Copyright-->
